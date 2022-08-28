9fb92ce30b744bd204f78f5b6e2d185c706c1759
Anderson Vieira <asvieira.br@gmail.com>
2022-08-28 00:08:51 -0300

    Exibe resultado em texto

--- main.js
+++ main.js
@@ -1,14 +1,19 @@
-let n1 = 5
+let n1 = document.querySelector('#usernumber').value;
 let n2 = Math.floor(Math.random() * 11);
+let result = document.querySelector('.result')
 
-sum = n1 + n2
 
-let sumString = sum.toString().split("")
+function parOuImpar() {
+    
+    let sum = n1 + n2
+    
+    let sumString = sum.toString().split("")
+    
+    const lastNumberSum = Number(sumString[sumString.length - 1]);
 
-const lastNumberSum = Number(sumString[sumString.length - 1]);
-
-if(lastNumberSum % 2 === 0){
-    console.log(`${sum} é par, porque o número ${lastNumberSum} é divisível por 2`);
-} else {
-    console.log(`${sum} é ímpar, porque o número ${lastNumberSum} não é divisível por 2.`);
+    if(lastNumberSum % 2 === 0){
+        result.innerHTML = `N1=${n1} - N2=${n2} - ${sum} é par`;
+    } else {
+        result.innerHTML = `N1=${n1} - N2=${n2} - ${sum} é ímpar`;
+    }
 }
